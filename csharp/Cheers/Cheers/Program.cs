0351655c3e42c6d6330fb402a76741cefff14eed
Martin Cross <mncross@gmail.com>
2016-03-11 14:11:30 -0600

    add birthday input prompt and countdown

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -13,8 +13,13 @@ namespace Cheers
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             name = name.ToLower();
-            
-                for (int i = 0; i < name.Length; i++)
+
+            Console.WriteLine("When is your birthday? (ex. 12/24)");
+            string birthdayInput = Console.ReadLine();
+            Console.WriteLine(Environment.NewLine);
+            string[] birthdayArray = birthdayInput.Split(new char[] { '/' });
+
+            for (int i = 0; i < name.Length; i++)
             {
                 char[] aArray = new char[] { 'b', 'c', 'd', 'g', 'j', 'k', 'p', 'q', 't', 'u', 'v', 'w', 'y', 'z' };
                 // char[] anArray = new char[] { 'a', 'e', 'f', 'h', 'i', 'l', 'm', 'n', 'o', 'r', 's', 'x' };
@@ -32,8 +37,45 @@ namespace Cheers
                     Console.Write(string.Format("Give me a {0}{1}", name[i], Environment.NewLine));
                 }
             }
-            Console.Write("{0} is.. GRAND!", name.ToUpper());
+            Console.WriteLine("{0} is.. GRAND!{1}", name.ToUpper(), Environment.NewLine);
+
+            DateTime today = DateTime.Today;
+            Parse parse = new Parse();
+            DateTime birthday = parse.ParseBirthday(today, birthdayArray);
+            TimeSpan birthdayDiff = birthday - today;
+            int tilBirthday = birthdayDiff.Days;
+
+            Console.WriteLine(String.Format("Your birthday is {0} days away!{1}", tilBirthday, Environment.NewLine));
+            if (tilBirthday == 0)
+            {
+                Console.WriteLine("Happy Birthday!!");
+            }
             Console.ReadKey();
+        }
+
+    }
+
+    public class Parse
+    {
+        public DateTime ParseBirthday(DateTime today, string[] birthdayArray)
+        {
+            int birthdayMonth;
+            int birthdayDate;
+            DateTime birthday;
+            if (int.TryParse(birthdayArray[0], out birthdayMonth) && int.TryParse(birthdayArray[1], out birthdayDate))
+            {
+                birthday = new DateTime(today.Year, birthdayMonth, birthdayDate);
+                if (today.CompareTo(birthday) > 0)
+                {
+                    birthday = birthday.AddYears(1);
+                    return birthday;
+                }
+                return birthday;
+            }
+            else
+            {
+                throw new Exception();
+            }
         }
     }
 }
