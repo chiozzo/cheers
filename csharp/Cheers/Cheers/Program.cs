c0cba557950b67c39a80fcfdc900ad1ba0a3fa72
Martin Cross <mncross@gmail.com>
2016-03-11 12:51:15 -0600

    refactor to use IndexOfAny

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -10,31 +10,19 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             name = name.ToLower();
-            for (int i = 0; i < name.Length; i++)
+            
+                for (int i = 0; i < name.Length; i++)
             {
-                /*
-                This is a bunch of garbage. I'd like to use name.IndexOfAny(aArray) == -1, but it consistently returns zero.
-                */
+                char[] aArray = new char[] { 'b', 'c', 'd', 'g', 'j', 'k', 'p', 'q', 't', 'u', 'v', 'w', 'y', 'z' };
+                // char[] anArray = new char[] { 'a', 'e', 'f', 'h', 'i', 'l', 'm', 'n', 'o', 'r', 's', 'x' };
                 bool needAn = false;
-                if (name[i] == 'a' ||
-                name[i] == 'e' ||
-                name[i] == 'f' ||
-                name[i] == 'h' ||
-                name[i] == 'i' ||
-                name[i] == 'l' ||
-                name[i] == 'm' ||
-                name[i] == 'n' ||
-                name[i] == 'o' ||
-                name[i] == 'r' ||
-                name[i] == 's' ||
-                name[i] == 'x')
+                if (name.IndexOfAny(aArray) == -1)
                 {
                     needAn = true;
                 }
-                // char[] anArray = new char[] { 'a', 'e', 'f', 'h', 'i', 'l', 'm', 'n', 'o', 'r', 's', 'x' };
-                // char[] aArray = new char[] { 'b', 'c', 'd', 'g', 'j', 'k', 'p', 'q', 't', 'u', 'v', 'w', 'y', 'z' };
                 if (needAn)
                 {
                     Console.Write(string.Format("Give me an {0}{1}", name[i], Environment.NewLine));
